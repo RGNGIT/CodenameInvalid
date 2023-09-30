00f0503009ff0b9dbeb205742bdbd28cc8ea09ca
RGNGIT <vnifedow@bk.ru>
2023-09-30 15:27:34 +0500

    Base

--- Assets/Config.cs
+++ Assets/Config.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public static class Config
+﻿public static class Config
 {
     public static string proto { get; set; } = "http://";
     public static string address { get; set; } = proto + "localhost/";
--- Assets/Scripts/Auth/AuthEvents.cs
+++ Assets/Scripts/Auth/AuthEvents.cs
@@ -5,12 +5,22 @@ public class AuthEvents : MonoBehaviour
 {
     public TMP_InputField login;
     public TMP_InputField password;
+    public TMP_Text info;
+
+    public void Awake()
+    {
+        info.text = string.Empty;
+    }
 
     public void Login() 
     {
         if(!string.IsNullOrEmpty(login.text) && !string.IsNullOrEmpty(password.text)) 
         {
             
+        }
+        else
+        {
+            info.text = "Не введен логин/пароль";
         }
     }
 }
